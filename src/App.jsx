83cb8051d3335b0999fdc411633fd335eef7a4e6
Oscar <ofvinals@gmail.com>
2024-03-09 11:22:16 -0300

    Guardar cambios locales en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { AppRouter } from '../router/AppRouter';
-import { ListadoTurnos } from './components/ListadoTurnos';
-import { PanelUsuarios } from './components/PanelUsuarios';
-
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 	return (
