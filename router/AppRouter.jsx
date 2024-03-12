fd9f15f0665e90d2611810e26cbddc53e46335e8
Michy <ricotero1309@gmail.com>
2024-03-12 20:30:36 -0300

    Mejorando Css Formularios

--- router/AppRouter.jsx
+++ router/AppRouter.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from '../src/components/Home.jsx';
 import { Clima } from '../src/components/Clima.jsx';
 import { Footer } from '../src/components/Footer.jsx';
+import { Login } from '../src/components/Login.jsx';
+import { Registro } from '../src/components/Registro.jsx';
+import { AuthProvider } from '../src/context/AuthContext.jsx';
 
 import { PanelUsuarios } from '../src/components/PanelUsuarios.jsx';
 import { ListadoTurnos } from '../src/components/ListadoTurnos.jsx';
@@ -15,20 +18,24 @@ import { GenerarPago } from '../src/components/GenerarPago.jsx';
 export const AppRouter = () => {
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route path='/' element={<Home />}></Route>
-				<Route path='/home' element={<Home />}></Route>
-				<Route path='/clima' element={<Clima />}></Route>
-				<Route path='/generarpago' element={<GenerarPago />}></Route>
-				<Route path='/panelusuarios' element={<PanelUsuarios />}></Route>
-				<Route path='/listadoturnos' element={<ListadoTurnos />}></Route>
-				<Route path='/pagosusuarios' element={<PagosUsuarios />}></Route>
-				<Route
-					path='/reservasusuario'
-					element={<ReservasUsuario />}></Route>
-				<Route path='/datosusuario' element={<DatosUsuario />}></Route>
-			</Routes>
-			<Footer />
+			<AuthProvider>
+				<Routes>
+					<Route path='/' element={<Home />}></Route>
+					<Route path='/home' element={<Home />}></Route>
+					<Route path='/clima' element={<Clima />}></Route>
+					<Route path='/generarpago' element={<GenerarPago />}></Route>
+					<Route path='/panelusuarios' element={<PanelUsuarios />}></Route>
+					<Route path='/listadoturnos' element={<ListadoTurnos />}></Route>
+					<Route path='/pagosusuarios' element={<PagosUsuarios />}></Route>
+					<Route path='/login' element={<Login />}></Route>
+					<Route path='/registro' element={<Registro />}></Route>
+					<Route
+						path='/reservasusuario'
+						element={<ReservasUsuario />}></Route>
+					<Route path='/datosusuario' element={<DatosUsuario />}></Route>
+				</Routes>
+				<Footer />
+			</AuthProvider>
 		</BrowserRouter>
 	);
 };
---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,114 @@
+import React, { useState, useEffect, createContext, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import { apiURL } from '/api/apiURL.js';
+// crea contexto
+const AuthContext = createContext();
+
+// funcion que retorna el contexto del objeto creado por useContext
+export const useAuth = () => {
+	const context = useContext(AuthContext);
+	if (!context) {
+		throw new Error('Error, no se creo el contexto!');
+	}
+	return context;
+};
+
+export const AuthProvider = ({ children }) => {
+	const [currentUser, setCurrentUser] = useState();
+	const [errors, setErrors] = useState([]);
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+	const [isLoading, setIsLoading] = useState(true);
+	const navigate = useNavigate();
+
+	// FUNCION REGISTRO DE USUARIOS
+	const registro = async (values) => {
+		try {
+			const res = await apiURL.post('/api/register', values);
+			if (res.status === 200) {
+				setCurrentUser(res.data);
+				setIsAuthenticated(true);
+				return res.data;
+			}
+		} catch (error) {
+			console.log(error.response.data);
+			setErrors(error.response.data);
+		}
+	};
+
+	// FUNCION LOGIN CON CORREO ELECTRONICO
+	const login = async (values) => {
+		try {
+			const res = await apiURL.post('/api/login', values, {
+				credentials: 'include',
+			});
+			document.cookie = `token=${res.data.token}; Path=/; `;
+			localStorage.setItem('token', res.data.token);
+			setIsAuthenticated(true);
+			setCurrentUser(res.data);
+			return res.data;
+		} catch (error) {
+			console.log(error.response.data);
+			setErrors(error.response.data);
+			throw error; // Re-lanzar el error para manejarlo en onSubmit
+		}
+	};
+
+	// FUNCION LOGOUT
+	const logout = async () => {
+		Cookies.remove('token');
+		localStorage.removeItem('token');
+		setCurrentUser(null);
+		setIsAuthenticated(false);
+	};
+
+	// Función de autenticacion de usuario logueado
+	useEffect(() => {
+		async function checkLogin() {
+			const cookies = Cookies.get();
+			if (!cookies.token) {
+				setIsAuthenticated(false);
+				setIsLoading(false);
+				return setCurrentUser(null);
+			}
+			try {
+				const res = await apiURL.get('/api/verify', {
+					withCredentials: true,
+					headers: {
+						Authorization: `Bearer ${cookies.token}`, // Agrega el token como encabezado de autorización
+					},
+				});
+				console.log(res);
+				if (!res.data) {
+					setIsAuthenticated(false);
+					setIsLoading(false);
+					return;
+				}
+				setIsAuthenticated(true);
+				setCurrentUser(res.data);
+				setIsLoading(false);
+			} catch (error) {
+				setIsAuthenticated(false);
+				setIsLoading(false);
+				setCurrentUser(null);
+			}
+		}
+		checkLogin();
+	}, []);
+
+	return (
+		<AuthContext.Provider
+			value={{
+				currentUser,
+				isAuthenticated,
+				registro,
+				login,
+				logout,
+				isLoading,
+			}}>
+			{children}
+		</AuthContext.Provider>
+	);
+};
+
+export default AuthContext;
